72265026d538a4f89b47f37bcb8eec34d1c6a31b
kozincov <32821146+kozincov@users.noreply.github.com>
2021-11-01 21:17:16 +0300

    Update build_a_pile_of_cubes.js

--- build_a_pile_of_cubes.js
+++ build_a_pile_of_cubes.js
@@ -1,11 +1,11 @@
 function findNb(m) {
     // your code
-  let total = 0;
+  let result = 0;
   let n =0;
   
-  while(total < m){
+  while(result < m){
     n+=1;
-    total += n**3;
+    result += n**3;
   }
-  return total === m ? n : -1;
+  return result === m ? n : -1;
 }
