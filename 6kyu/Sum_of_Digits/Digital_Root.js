80b3c4434db6605146d541db25f6b057c798aed3
kozincov <kozincov1998@gmail.com>
2022-09-26 18:23:15 +0300

    create function digitalRoot

---
+++ 6kyu/Sum_of_Digits/Digital_Root.js
@@ -0,0 +1,13 @@
+function digitalRoot(n) {
+    let arr = ('' + n).split('').map(Number)
+    if (arr.length === 1) {
+        return n
+    }
+    return digitalRoot(arr.reduce((a,b)=> a+b))
+}
+
+console.log(digitalRoot(166))
+// 16  -->  1 + 6 = 7
+// 942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
